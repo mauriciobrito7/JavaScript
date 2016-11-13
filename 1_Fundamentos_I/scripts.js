ede99c3a8a05f3e1fa3411a80f79fb37490f355c
Mauricio <maubot01@gmail.com>
2016-11-13 15:41:21 -0430

    Fundamentos I

---
+++ 1_Fundamentos_I/scripts.js
@@ -0,0 +1,26 @@
+// Quitar el siguiente alert en produccion
+// alert("Estoy aprendiendo JavaScript");
+
+
+nombre = "Profesor";
+var alumno = "Carlos";
+var profesor = "Ruben";
+
+function saludar(){
+  alert("Buenos días");
+}
+
+var miObjeto = {
+  nombre: "Objeto",
+  edad: 10
+};
+
+var despedirse = function(){
+  alert('Hasta mañana');
+};
+
+var noDefinida = "Esta variable ya ha sido definida";
+//alert(noDefinida);
+
+var cita = "\"Solo sé que nada sé\" por Socrates";
+alert(cita);
